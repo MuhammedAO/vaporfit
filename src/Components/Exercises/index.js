e2889810e46642497bc9f8415c99d51b14165d19
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-02-10 17:38:50 +0100

    Dynamically select exercise categories

--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -6,52 +6,62 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 const styles = {
- Paper: {
-  padding: 20, 
-  marginTop: 10, 
-  marginBottom: 10,
-  height:500, 
-  overflowY: 'auto'
- }
+	Paper: {
+		padding: 20,
+		marginTop: 10,
+		marginBottom: 10,
+		height: 500,
+		overflowY: 'auto'
+	}
 }
 
-export default function index({ exercises }) {
- return (
+export default function index({ exercises, category }) {
+	return (
   <Grid container>
-   <Grid item sm>
-    <Paper style={styles.Paper}>
-     {exercises.map(([group, exercises]) =>
-      <React.Fragment>
-       <Typography
-        variant="h5"
-        style={{ textTransform: 'capitalize' }}
-       >
-        {group}
-       </Typography>
-       <List component="ul" aria-label="secondary mailbox folders">
-        {exercises.map(({title}) =>
-         <ListItem button>
-         <ListItemText primary={title} />
-        </ListItem>
+    <Grid item sm>
+      <Paper style={styles.Paper}>
+
+        {exercises.map(([muscles, exercises]) =>
+
+          !category || category === muscles
+
+            ? 	
+            <React.Fragment>
+              <Typography
+                variant="h5"
+                style={{ textTransform: 'capitalize' }}
+              >
+                {muscles}
+              </Typography>
+              <List component="ul" aria-label="secondary mailbox folders">
+                {exercises.map(({ title }) =>
+                  <ListItem button>
+                    <ListItemText 
+                    primary={title} 
+                    />
+                  </ListItem>
+                )}
+              </List>
+            </React.Fragment> 
+            
+            : null
+
         )}
-       </List>
-      </React.Fragment>
-     )}
-    </Paper>
-   </Grid>
-   <Grid item sm>
-    <Paper style={styles.Paper}>
-     <Typography variant="h6">
-       Welcome
-     </Typography>
-     <Typography variant="subtitle1"
-     style={{marginTop:20}}
-     >
-        Please select an exercise from the list on the left
-     </Typography>
       </Paper>
-   </Grid>
+    </Grid>
+    <Grid item sm>
+      <Paper style={styles.Paper}>
+        <Typography variant="h6">
+          Welcome
+    </Typography>
+        <Typography variant="subtitle1"
+          style={{ marginTop: 20 }}
+        >
+          Please select an exercise from the list on the left
+    </Typography>
+      </Paper>
+    </Grid>
 
   </Grid>
- )
+	)
 }
