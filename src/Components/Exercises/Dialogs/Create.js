0c34e6ddc45cc581f84dba09473339b40cd3272c
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-02-13 01:12:33 +0100

    textareas and form input logic

--- src/Components/Exercises/Dialogs/Create.js
+++ src/Components/Exercises/Dialogs/Create.js
@@ -7,11 +7,30 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import {Add} from '@material-ui/icons';
 import Fab from '@material-ui/core/Fab';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import { withStyles } from '@material-ui/core/styles'
 
+
+
+const styles = theme =>  ({
+  formControl:{
+    width:500
+  }
+ })
 class Create extends React.Component {
   state = {
     open: false,
+    exercise:{
+    title:'',
+    description:'',
+    muscles:''
+    }
   }
+
 
   handleToggle = () => {
     this.setState({
@@ -19,8 +38,37 @@ class Create extends React.Component {
     })
   }
 
+  handleChange = name => ({target:{ value}}) => {
+   this.setState({
+     exercise:{
+      ...this.state.exercise,
+      [name] :  value
+     }
+   })
+  }
+
+  handleSubmit = () => {
+    const {exercise} = this.state
+
+    this.props.onCreate({
+      ...exercise,
+      id: exercise.title.toLowerCase().replace(/ /g, '-')
+    })
+
+    this.setState({
+      open:false,
+      exercise: {
+        title:'',
+        description:'',
+        muscles:''
+      }
+    })
+  }
+
   render() {
-    const {open} = this.state
+    const {open, exercise:{title, description, muscles}} = this.state
+
+    const {classes, muscles : categories} = this.props
     return (
       <React.Fragment>
       <Fab 
@@ -38,11 +86,43 @@ class Create extends React.Component {
        Fill out the form below
       </DialogContentText>
        <form>
-
+       <TextField
+          value={title}
+          label="Title"
+          onChange={this.handleChange('title')}
+          className={classes.formControl}
+        />
+        <br/>
+        <FormControl className={classes.formControl}>
+        <InputLabel>Muscles</InputLabel>
+        <Select
+          value={muscles}
+          name="muscles"
+          onChange={this.handleChange('muscles')}
+          
+        >
+        {categories.map(category =>
+          <MenuItem key={category} value={category}>{category}</MenuItem>
+        )}
+        </Select>
+      </FormControl>
+        <br/>
+       <TextField
+          value={description}
+          label="Description"
+          onChange={this.handleChange('description')}
+          multiline
+          rows='4'
+          className={classes.formControl}
+        />
        </form>
     </DialogContent>
     <DialogActions>
-      <Button color="primary" variant="contained">
+      <Button 
+      color="primary" 
+      variant="contained"
+      onClick={this.handleSubmit}
+      >
         Create
       </Button>
     </DialogActions>
@@ -53,4 +133,4 @@ class Create extends React.Component {
     
 }
 
-export default Create
+export default withStyles(styles)(Create)
