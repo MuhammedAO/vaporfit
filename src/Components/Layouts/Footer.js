9d28b8e5511bb924a34db4f3c6eeab03e69af990
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-03-31 22:24:37 +0100

    refractoring

--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -3,26 +3,45 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { withWidth } from '@material-ui/core'
+import { withContext } from '../../context'
 
-export default withWidth()(function Footer({muscles, category, onSelect, width}) {
-  const index = category ? muscles.findIndex(muscles => muscles === category) + 1 : 0
 
- const onIndexSelect = (e,index) => onSelect(index === 0 ? '' : muscles[index -1 ])
+class Footer extends React.Component {
+
+  onIndexSelect = (e, index) => {
+    const { onCategorySelect, muscles } = this.props
+    onCategorySelect(index === 0 ? '' : muscles[index - 1])
+
+  }
+  getIndex = () => {
+    const { category, muscles } = this.props
+    return category ? muscles.findIndex(muscles => muscles === category) + 1 : 0
+
+  }
+
+  render() {
+    const { width, muscles } = this.props
     return (
-    <AppBar position = 'static'>
-  <Tabs
-    value={index}
-    onChange={onIndexSelect}
-    indicatorColor="secondary"
-    textColor="secondary"
-    centered={width !== 'xs'}
-    // scrollable={width === 'xs'}
-  >
-  <Tab label="All"/>
-  {muscles.map(muscle =>
-    <Tab key={muscle} label={muscle} />
-   )}
-  </Tabs>
-</AppBar>
+      <AppBar position='static'>
+        <Tabs
+          value={this.getIndex()}
+          onChange={this.onIndexSelect}
+          indicatorColor="secondary"
+          textColor="secondary"
+          centered={width !== 'xs'}
+        // scrollable={width === 'xs'}
+        >
+          <Tab label="All" />
+          {muscles.map(muscle =>
+            <Tab key={muscle} label={muscle} />
+          )}
+        </Tabs>
+      </AppBar>
     )
-})
+  }
+
+
+}
+
+
+export default withContext(withWidth()(Footer))
